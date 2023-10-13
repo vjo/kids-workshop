f04d8a91f2d47e53e3bf2b819939bbc7362ee4f5
Arnaud Rinquin <rinquin.arnaud@gmail.com>
2023-10-14 00:25:22 +0200

    feat: Design v4

--- src/screens/kids.tsx
+++ src/screens/kids.tsx
@@ -7,10 +7,12 @@ export default function Kids() {
     <main className="">
       <h1>Kids</h1>
       {kids.map((kid) => (
-        <Link key={kid.id} to={`/kids/${kid.id}`}>
-          {kid.name}
-          <img src={kid.photoUrl} alt={kid.name} />
-        </Link>
+        <div>
+          <Link key={kid.id} to={`/kids/${kid.id}`}>
+            {kid.name}
+            <img src={kid.photoUrl} alt={kid.name} width="200" />
+          </Link>
+        </div>
       ))}
     </main>
   );
