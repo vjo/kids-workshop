a6cb6907dc0e05a1b20197707f6b2c0c9c21f4d1
Arnaud Rinquin <rinquin.arnaud@gmail.com>
2023-10-14 00:10:13 +0200

    chore: make it PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import { VitePWA } from "vite-plugin-pwa";
 import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
@@ -5,7 +6,31 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   envPrefix: "KW_",
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "mask-icon.svg"],
+      manifest: {
+        name: "Kids & Workshops",
+        short_name: "K&W",
+        description: "Utility app to handle kids and their favorites workshops",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
