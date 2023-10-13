ef944d6cfffcc689dc1e548e3a391f06221ce13a
Arnaud Rinquin <rinquin.arnaud@gmail.com>
2023-10-14 00:18:01 +0200

    feat: Design v3

--- src/screens/home.tsx
+++ src/screens/home.tsx
@@ -3,8 +3,14 @@ import { Link } from "react-router-dom";
 export default function Home() {
   return (
     <main className="">
-      <Link to="/kids">Kids</Link>
-      <Link to="/workshops">Workshops</Link>
+      <ul>
+        <li>
+          <Link to="/kids">Kids</Link>
+        </li>
+        <li>
+          <Link to="/workshops">Workshops</Link>
+        </li>
+      </ul>
     </main>
   );
 }
